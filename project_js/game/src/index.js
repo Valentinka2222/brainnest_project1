4612b91f6c4ae448c58f1338dbd82a7db1a42477
Valentinka2222 <xamalyan89@gmail.com>
2023-02-10 00:28:28 +0200

    refactoring

--- project_js/game/src/index.js
+++ project_js/game/src/index.js
@@ -10,14 +10,12 @@ import showResults from './showResults.js'
 import computerPlay from './computerPlay.js'
 
 createElements()
-const h1 = document.createElement('h1')
 
 const playRound = (e) => {
   const h1Element = document.querySelector('h1')
   h1Element.classList.remove('hidden')
 
   let {computerChoice, playerChoice} = choice
-  let {computerPoints, playerPoints} = points
 
   playerChoice = e.target.dataset.item
   computerChoice = computerPlay()
@@ -28,16 +26,16 @@ const playRound = (e) => {
     (playerChoice === 'scissors' && computerChoice === 'rock')
   ) {
     points.computerPoints++
-    spanPlayerPoints.textContent = `Player ${playerPoints}`
-    spanComputerPoints.textContent = `Computer ${computerPoints}`
+    spanPlayerPoints.textContent = `Player ${points.playerPoints}`
+    spanComputerPoints.textContent = `Computer ${points.computerPoints}`
   } else if (
     (playerChoice === 'rock' && computerChoice === 'scissors') ||
     (playerChoice === 'paper' && computerChoice === 'rock') ||
     (playerChoice === 'scissors' && computerChoice === 'paper')
   ) {
     points.playerPoints++
-    spanPlayerPoints.textContent = `Player ${playerPoints}`
-    spanComputerPoints.textContent = `Computer ${computerPoints}`
+    spanPlayerPoints.textContent = `Player ${points.playerPoints}`
+    spanComputerPoints.textContent = `Computer ${points.computerPoints}`
   }
 
   spanComputerChoice.textContent = `Computer chose ${computerChoice}!`
