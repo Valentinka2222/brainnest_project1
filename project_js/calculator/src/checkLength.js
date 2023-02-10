92e3aeb48cd45e369ea9b48d68f0c1100142500c
Valentinka2222 <xamalyan89@gmail.com>
2023-02-10 20:01:49 +0200

    change error message

--- project_js/calculator/src/checkLength.js
+++ project_js/calculator/src/checkLength.js
@@ -2,8 +2,8 @@ const checkLength = (number) => {
   let display = document.querySelector('.currentNumb')
 
   if (number.length > 17)
-    display.textContent =
-      "Calculator don't work with numbers more than 99999999999"
+    display.textContent = "Calculator doesn't work with big numbers!"
+
   if (number.length > 7 && number.length < 11) display.style.fontSize = '40px'
   else if (number.length >= 11) display.style.fontSize = '30px'
   else display.style.fontSize = '55px'
