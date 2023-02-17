9b4dcbb28ed4703bc8a9c4cd69a0165fe0ecc39b
Valentinka2222 <xamalyan89@gmail.com>
2023-02-17 10:47:53 +0200

    add else if in function showresults

--- project_js/game/src/showResults.js
+++ project_js/game/src/showResults.js
@@ -4,17 +4,17 @@ import {spanComputerChoice} from './createElements.js'
 
 const showResults = (computerPoints, playerPoints) => {
   const h1 = document.querySelector('h1')
-  if (computerPoints == 5) {
+  if (computerPoints === 5) {
     points.computerPoints = 0
     points.playerPoints = 0
     spanComputerChoice.textContent = `Computer win!`
     h1.classList.add('hidden')
-  }
-  if (playerPoints == 5) {
+  } else if (playerPoints === 5) {
     points.computerPoints = 0
     points.playerPoints = 0
     spanComputerChoice.textContent = `Player win!`
     h1.classList.add('hidden')
   }
+  return points
 }
 export default showResults
